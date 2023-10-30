cbd653859d62d575be3fef87227610700a23f7bc
gerald.h.jones94@gmail.com <gerald.h.jones94@gmail.com>
2023-10-29 22:13:23 -0500

    Refactor and cleanup triangle files

--- Graphics/triangle.c
+++ Graphics/triangle.c
@@ -3,7 +3,16 @@
 #include "triangle.h"
 
 
+//============================================================================
 
+//	PRIVATE PROTOTYPES
+
+//============================================================================
+static void sort_triangle_vertices(vec2_t* vertices);
+static void fill_flat_bottom_triangle(vec2_t v0, vec2_t v1, vec2_t m, uint32_t color);
+static void fill_flat_top_triangle(vec2_t v1, vec2_t m, vec2_t v2, uint32_t color);
+static void swap(vec2_t* v1, vec2_t* v2);
+//============================================================================
 
 /// <summary>
 /// Fills out a triangle (Post Projection of vertices)
@@ -34,8 +43,8 @@ void fill_triangle(triangle_t triangle, uint32_t color)
 	// 
 	//==================================================================================
 
-	//sort by y coordinate
-	sort_triangle_vertices(&(triangle.points));
+	//sort by y coordinate so that v1 is the middle height
+	sort_triangle_vertices(triangle.points);
 
 	vec2_t v0 = triangle.points[0];
 	vec2_t v1 = triangle.points[1];
@@ -43,6 +52,7 @@ void fill_triangle(triangle_t triangle, uint32_t color)
 	vec2_t m;
 
 	m.y = v1.y;
+
 	//solve for mx using similar triangles (mx-x0 / x2 - x0) = ( y1-y0 / y2-y0 )
 	m.x = ( ((v2.x - v0.x) * (v1.y - v0.y)) / (v2.y - v0.y) ) + v0.x;
 
@@ -50,11 +60,12 @@ void fill_triangle(triangle_t triangle, uint32_t color)
 	fill_flat_top_triangle(v1, m, v2, color);
 }
 
+
 /// <summary>
-/// Sorts the triangle vertices by the y coordinate
+/// Sorts vertices from least to greatest by the y coordinate
 /// </summary>
-/// <param name="triangle"></param>
-void sort_triangle_vertices(vec2_t *vertices)
+/// <param name="vertices"></param>
+static void sort_triangle_vertices(vec2_t *vertices)
 {
 	// Swap vertices into ascending order v0.y < v1.y < v2.y
 	vec2_t v0 = vertices[0];
@@ -84,7 +95,14 @@ void sort_triangle_vertices(vec2_t *vertices)
 	vertices[2] = v2;
 }
 
-void fill_flat_bottom_triangle(vec2_t v0, vec2_t v1, vec2_t m, uint32_t color)
+/// <summary>
+/// Fill flat bottom triangle
+/// Starts from top going to bottom
+/// </summary>
+/// <param name="v0"></param>
+/// <param name="v1"></param>
+/// <param name="m"> => m.y = v1.y, (solve for m.x)</param>
+static void fill_flat_bottom_triangle(vec2_t v0, vec2_t v1, vec2_t m, uint32_t color)
 {
 	//======================================
 	//
@@ -119,12 +137,35 @@ void fill_flat_bottom_triangle(vec2_t v0, vec2_t v1, vec2_t m, uint32_t color)
 	}
 }
 
-void fill_flat_top_triangle(vec2_t v1, vec2_t m, vec2_t v2, uint32_t color)
+/// <summary>
+/// Fill flat top triangle
+/// (Starts from bottom going up)
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="m"> => m.y = v1.y, (solve for m.x)</param>
+/// <param name="v2"></param>
+static void fill_flat_top_triangle(vec2_t v1, vec2_t m, vec2_t v2, uint32_t color)
 {
+	int y1 = (int)v1.y;
+	int y2 = (int)v2.y;
 
+	float changeInY = v2.y - v1.y;
+
+	float slope1_increment = (float)(v1.x - v2.x) / changeInY;
+	float slope2_increment = (float)(m.x - v2.x) / changeInY;
+
+	float start_x = v2.x;
+	float end_x = v2.x;
+
+	for (int y = y2; y >= v1.y; y--)
+	{
+		draw_line((int)start_x, y, (int)end_x, y, color);
+		start_x += slope1_increment; //calculate new startX
+		end_x += slope2_increment; //calculate new endX
+	}
 }
 
-void swap(vec2_t *v1, vec2_t *v2)
+static void swap(vec2_t *v1, vec2_t *v2)
 {
 	vec2_t swap = *v1;
 
--- Graphics/triangle.h
+++ Graphics/triangle.h
@@ -9,27 +9,3 @@ typedef struct
 } triangle_t;
 
 void fill_triangle(triangle_t triangle, uint32_t color);
-/// <summary>
-/// Sorts vertices from least to greatest by the y coordinate
-/// </summary>
-/// <param name="vertices"></param>
-void sort_triangle_vertices(vec2_t* vertices);
-
-
-/// <summary>
-/// Fill flat bottom triangle
-/// </summary>
-/// <param name="v0"></param>
-/// <param name="v1"></param>
-/// <param name="m"> => m.y = v1.y, (solve for m.x)</param>
-void fill_flat_bottom_triangle(vec2_t v0, vec2_t v1, vec2_t m, uint32_t color);
-
-/// <summary>
-/// Fill flat top triangle
-/// </summary>
-/// <param name="v1"></param>
-/// <param name="m"> => m.y = v1.y, (solve for m.x)</param>
-/// <param name="v2"></param>
-void fill_flat_top_triangle(vec2_t v1, vec2_t m, vec2_t v2, uint32_t color);
-
-void swap(vec2_t* v1, vec2_t* v2);
